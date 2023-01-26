83c5bef9977061148150c7849af1daed9c5f98ec
EricFG <eric_fg@live.com.ar>
2023-01-26 11:16:15 -0300

    update desafio

--- script-desafio.sql
+++ script-desafio.sql
@@ -1,8 +1,12 @@
 #CONSULTAS
-SELECT * FROM EmployeeTraining;
-SELECT * FROM OfficeTraining;
+SELECT * FROM EmployeeTraining; 
+#et.
+SELECT * FROM OfficeTraining; 
+#ot.
 SELECT * FROM SectorTraining; 
-SELECT * FROM EmployeeTrainingContact;
+#st.
+SELECT * FROM EmployeeTrainingContact; 
+#etc.
 
 #1
 SELECT Name, AdmissionDate, ot.Description, st.Description  
@@ -34,6 +38,70 @@ WHERE st.Id = 4
 Order By Wage desc;
 
 #5
+SELECT Name, max(Wage) as MaiorSalario, st.Description as Setor
+FROM EmployeeTraining as et
+INNER JOIN OfficeTraining as ot ON ot.Id = et.OfficeId
+INNER JOIN SectorTraining as st ON st.Id = ot.SectorId
+Group By ot.SectorId;
 
 #6
+SELECT Name, min(Wage) as MenorSalario, st.Description as Setor
+FROM EmployeeTraining as et
+INNER JOIN OfficeTraining as ot ON ot.Id = et.OfficeId
+INNER JOIN SectorTraining as st ON st.Id = ot.SectorId
+Group By ot.SectorId;
 
+#7
+SELECT et.Id, Name, ot.Description, ec.Contact 
+FROM EmployeeTraining as et
+INNER JOIN OfficeTraining as ot ON ot.Id = et.OfficeId
+INNER JOIN EmployeeTrainingContact as etc ON etc.Id = ec.EmployeeTrainingId
+Group By etc.Id;
+
+#8
+SELECT * FROM EmployeeTraining 
+WHERE AdmissionDate between '2010-01-01' and '2020-12-31' 
+Order By AdmissionDate;
+
+#9
+SELECT Name, ot.Description, IndicatedBy 
+FROM EmployeeTraining as et 
+INNER JOIN OfficeTraining as ot ON  ot.Id = et.OfficeId
+INNER JOIN SectorTraining as st ON st.Id = ot.SectorId
+Order By IndicatedBy desc;
+
+
+#10
+SELECT Name, year(curdate()) - year(AdmissionDate) as EmployeeTime 
+FROM EmployeeTraining where year(curdate()) - year(AdmissionDate) >= 10;
+
+#11
+#11.1 Item 1 para consultar os que tem contato
+SELECT Name, AdmissionDate, ot.Description, et.Id 
+FROM EmployeeTraining as et
+INNER JOIN OfficeTraining as ot ON ot.Id = et.OfficeId
+INNER JOIN EmployeeTrainingContact as etc ON et.Id = etc.EmployeeTrainingId
+Group By etc.Id;
+
+#11.2 Item 2 para dar o resultado da questão
+SELECT  et.Id, et.Name, ot.Description 
+FROM EmployeeTraining et
+INNER JOIN OfficeTraining ot ON ot.id = et.OfficeId
+LEFT JOIN EmployeeTrainingContact etc ON etc.EmployeeTrainingId = et.id
+WHERE etc.id IS NULL;
+
+#12
+SELECT et.Name, ot.Description, et.Contact
+FROM EmployeeTraining et
+INNER JOIN OfficeTraining ot ON ot.id = et.OfficeId
+LEFT JOIN EmployeeTrainingContact etc ON etc.EmployeeTrainingId = et.id
+WHERE etc.Contact = 1;
+
+#13
+SELECT st.description, SUM(et.Wage) as salarios 
+FROM SectorTraining st
+INNER JOIN OfficeTraining ot ON ot.SectorId = st.id
+INNER JOIN EmployeeTraining et ON et.OfficeId = ot.id
+GROUP BY st.id, et.wage;
+
+#14
